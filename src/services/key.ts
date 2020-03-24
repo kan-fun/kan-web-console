5287fc6b835800dadc2df1795dc0f4e9725ed858
Your Name <you@example.com>
2020-03-24 21:38:42 +0800

    Fix ENDPOINT

--- src/services/key.ts
+++ src/services/key.ts
@@ -1,6 +1,6 @@
 import request from '@/utils/request';
 
-const ENDPOINT = "http://api.mlflow.org.cn";
+const ENDPOINT = "http://58b5dd3da8514f30a8dfbf42bb0a740c-cn-beijing.alicloudapi.com";
 const view_key_url = `${ENDPOINT}/view-key`;
 
 export async function fetch() {
