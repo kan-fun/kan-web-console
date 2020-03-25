56a00892189cd7aca85b3b718b34dcf951207251
Your Name <you@example.com>
2020-03-25 10:17:23 +0800

    Restore deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 wget http://gosspublic.alicdn.com/ossutil/1.6.10/ossutil64
 chmod 755 ossutil64
 ./ossutil64 config -e oss-cn-beijing.aliyuncs.com -i $ACCESSKEY -k $SECRETKEY
-./ossutil64 rm -rf oss://kan-web-index/console
-./ossutil64 mkdir -rf oss://kan-web-index/console
-./ossutil64 cp -rf ./dist/ --include "*"  oss://kan-web-index/console
+./ossutil64 rm -rf oss://kan-web-console/
+./ossutil64 cp -rf ./dist/ --include "*"  oss://kan-web-console/
